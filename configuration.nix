7d83e852c6c06bf3e8842bdb3febd0f9d1be157f
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-01-31 10:23:19 +0000

    Disk enc

--- configuration.nix
+++ configuration.nix
@@ -34,6 +34,14 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.initrd.luks.devices = {
+    crypted = {
+      device = "/dev/disk/by-uuid/d0ad36ad-4630-4764-ae08-a8c3e788d521";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+
   networking.hostName = "finch"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
