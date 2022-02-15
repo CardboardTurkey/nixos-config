e8aaf757ecf252de44590cb6b0dddc14161cb467
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-02-15 15:08:32 +0000

    Maybe fix touchpad

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,8 @@ in
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
+  services.xserver.libinput.tapping = true;
+  services.xserver.libinput.clickMethod = "buttonareas";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kiran = {
