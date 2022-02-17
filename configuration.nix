40482275b03bffa159def0a3c9dc222f04e9f2d2
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-02-17 10:27:41 +0000

    Try kernal param for touchpad click

--- configuration.nix
+++ configuration.nix
@@ -42,6 +42,9 @@ in
     };
   };
 
+  # Maybe fix touchpad?
+  boot.kernelParams = [ "psmouse.synaptics_intertouch=0" ];
+
   networking.hostName = "finch"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
