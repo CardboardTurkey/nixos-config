301fa5876cb01326c70a3ba1bc007a09a8b281a2
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-03-10 18:36:28 +0000

    remove useDHCP

--- configuration.nix
+++ configuration.nix
@@ -49,9 +49,6 @@ in
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
-  # networking.interfaces.enp0s20f0u2u1.useDHCP = true;
-  networking.interfaces.enp0s31f6.useDHCP = true;
-  networking.interfaces.wlp0s20f3.useDHCP = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
