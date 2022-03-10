85ab446a4b1e4fa1078c7c34f1ad4f023fe6bc45
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-03-10 10:00:53 +0000

    Switch i3 config to home-manager

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@
 # test
 # https://gist.github.com/martijnvermaat/76f2e24d0239470dd71050358b4d5134
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
@@ -22,6 +22,19 @@ let
         sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
       }
     ];
+  # i3
+  mod = "Mod4";
+  alt = "Mod1";
+  ws-term = "0";
+  ws-code = "1";
+  ws-fire = "2";
+  ws-spot = "3";
+  ws-pdf = "4";
+  ws-mail = "5";
+  ws-img = "6";
+  ws-irc = "7";
+  ws-chrm = "8";
+
 in
 
 {
@@ -144,8 +157,56 @@ in
         }
       '';
     };
-    xdg = {
-      configFile."i3".source = ./config/i3;
+    xsession.windowManager.i3 = {
+      enable = true;
+      config = {
+        modifier = mod;
+        bars = [ ];
+        fonts = {
+          names = [ "DejaVu Sans Mono" "FontAwesome5Free" ];
+          style = "Bold Semi-Condensed";
+          size = 11.0;
+        };
+        keybindings = lib.mkOptionDefault {
+          "XF86AudioMute"         = "exec amixer set Master toggle";
+          "XF86AudioLowerVolume"  = "exec amixer set Master 4%-";
+          "XF86AudioRaiseVolume"  = "exec amixer set Master 4%+";
+          "XF86MonBrightnessDown" = "exec brightnessctl set 4%-";
+          "XF86MonBrightnessUp"   = "exec brightnessctl set 4%+";
+          "${mod}+Control+Return" = "exec termite";
+          "${mod}+Return"         = "workspace 1; exec pgrep termite || termite";
+          "${mod}+Control+space"  = "focus mode_toggle";
+          "${mod}+Control+Left"   = "workspace prev";
+          "${mod}+Control+Right"  = "workspace next";
+          "${mod}+$alt+Down"      = "move workspace to output down";
+          "${mod}+$alt+Up"        = "move workspace to output up";
+          "${mod}+$alt+Left"      = "move workspace to output left";
+          "${mod}+$alt+Right"     = "move workspace to output right";
+        };
+        assigns = {
+          "${ws-code}" = [{ class="VSCodium";}];
+          "${ws-fire}" = [{ class="Firefox";}];
+          "${ws-chrm}" = [{ class="Google-chrome";}];
+          "${ws-spot}" = [{ class="spotify";}];
+          "${ws-pdf}"  = [{ class="Evince";}];
+          "${ws-mail}" = [{ class="Mail";}];
+          "${ws-img}"  = [{ class="viewnior";}];
+          "${ws-irc}"  = [{ title="quassel";}];        
+        };
+        startup = [
+          { command = "nitrogen --restore"; always = true; notification = false; }
+        ];
+      }; 
+      extraConfig = ''
+        for_window [class="Mail"] focus
+        for_window [class="vscodium"] focus
+        for_window [class="firefox"] focus
+        for_window [class="viewnior"] focus
+        for_window [class="Evince"] focus   
+
+        default_border pixel 1
+        default_floating_border pixel 1
+      '';
     };
     programs.tmux = {
       enable = true;
