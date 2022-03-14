639d16dee9c8429e8fadd9a81431afe9f5b42d2c
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-03-14 17:30:37 +0000

    Add lock binding

--- home/graphical.nix
+++ home/graphical.nix
@@ -77,6 +77,7 @@ in
           "XF86AudioRaiseVolume"  = "exec amixer set Master 4%+";
           "XF86MonBrightnessDown" = "exec brightnessctl set 4%-";
           "XF86MonBrightnessUp"   = "exec brightnessctl set 4%+";
+          "${mod}+L"              = "exec betterlockscreen -l dim";
           "${mod}+Control+Return" = "exec termite";
           "${mod}+Return"         = "workspace 1; exec pgrep termite || termite";
           "${mod}+Control+space"  = "focus mode_toggle";
