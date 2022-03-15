d77b8c5e1771df20b3f808b178a6670c6b01302e
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-03-15 10:13:49 +0000

    Fixup polybar fonts

--- home/polybar.nix
+++ home/polybar.nix
@@ -47,13 +47,11 @@ in
           module-margin-left = "1";
           module-margin-right = "2";
 
-          font-0 = "Bitstream Vera Sans:style=Roman:pixelsize=13;3";
-          font-1 = "Bitstream Vera Sans:style=Roman:pixelsize=13;3";
-          font-2 = "Font Awesome 5 Free,Font Awesome 5 Free Regular:style=Regular:size=13;3";
-          font-3 = "Font Awesome 5 Free,Font Awesome 5 Free Solid:style=Solid:size=13;3";
-          font-4 = "Font Awesome 5 Brands,Font Awesome 5 Brands Regular:style=Regular:style=Solid:size=13;3";
-          font-5 = "Hack:pixelsize=13;2";
-          font-6 = "Noto Color Emoji:style=Regular:scale=8;2";
+          font-0 = "DejaVuSansMono Nerd Font Mono:style=Book:pixelsize=13;3";
+          font-1 = "Font Awesome 5 Free,Font Awesome 5 Free Regular:style=Regular:size=13;3";
+          font-2 = "Font Awesome 5 Free,Font Awesome 5 Free Solid:style=Solid:size=13;3";
+          font-3 = "Font Awesome 5 Brands,Font Awesome 5 Brands Regular:style=Regular:style=Solid:size=13;3";
+          font-4 = "Noto Color Emoji:style=Regular:scale=8;2";
 
           modules-left = "i3";
           modules-center = "player-mpris-tail";
