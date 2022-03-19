ad10130d593e2abc5246bc5f4f483404b774bb47
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-03-19 09:03:45 +0000

    Try exclude i3lock from opacity

--- home/graphical.nix
+++ home/graphical.nix
@@ -132,6 +132,7 @@ in
       blur = true;
       fade = true;
       inactiveOpacity = "0.8";
+      opacityRule = [ "100:name *= 'i3lock'" ];
       experimentalBackends = true;
       extraOptions = ''
         blur:
