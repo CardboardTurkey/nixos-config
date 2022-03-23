b53136d893a501340fcf723df625cb5327341996
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-03-23 16:57:48 +0000

    Set laptop fingerprint screen in core

--- core/hardware.nix
+++ core/hardware.nix
@@ -23,5 +23,10 @@ with lib;
       type = with types; uniq str;
       description = "As given in /sys/class/power_supply/";
     };
+    edp1 = mkOption {
+      default = "00ffffffffffff005a6322f5010101010814010380341d782eeed5a555489b26125054bfef80d1c0b300a9409500904081808140714f023a801871382d40582c450009252100001e000000ff005239533130303830303333330a000000fd00324b165211000a202020202020000000fc00564732343237574d0a2020202000bd";
+      type = with types; uniq str;
+      description = "Fingerprint for laptop screen";
+    };
   };
 }
