a206fa8bb0e7a023656247c1334aa58a745ad33e
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-04-04 17:17:44 +0100

    Add lockscreen to activation script

--- user-programs/graphical/wallpaper.nix
+++ user-programs/graphical/wallpaper.nix
@@ -7,8 +7,12 @@
   home-manager.users.kiran = { pkgs, lib, ... }: {
     home.activation = {
       wallpaper-setup = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        $DRY_RUN_CMD [ -f "$HOME/.background-image" ] || \
-        wget $VERBOSE_ARG https://w.wallhaven.cc/full/lm/wallhaven-lmjzjr.jpg -O "$HOME/.background-image"
+        if ! [ -f "$HOME/.background-image" ] 
+        then
+          $DRY_RUN_CMD wget $VERBOSE_ARG https://w.wallhaven.cc/full/lm/wallhaven-lmjzjr.jpg -O "$HOME/.background-image"
+          $DRY_RUN_CMD feh --bg-fill $HOME/.background-image
+          $DRY_RUN_CMD betterlockscreen -u ~/.background-image
+        fi
       '';
     };
   };
