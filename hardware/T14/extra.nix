9b3a95b67b8e56873f024b7184cf8bd9e418021d
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-04-09 14:41:10 +0100

    Add gimp

--- hardware/T14/extra.nix
+++ hardware/T14/extra.nix
@@ -2,9 +2,12 @@
 
 {
 
-boot.initrd.availableKernelModules = [ "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "usbhid" "usb_storage" "sd_mod" ];
 
-# For touch-to-click
-boot.kernelParams = [ "psmouse.synaptics_intertouch=0" ];
+  # For touch-to-click
+  boot.kernelParams = [ "psmouse.synaptics_intertouch=0" ];
 
+  environment.systemPackages = with pkgs; [
+    gimp
+  ];
 }
