765c4ea5aa95051e30cdbcb8fe24750226d002c7
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-05-11 22:51:22 +0100

    Starship icons

--- user-config/terminal/starship.nix
+++ user-config/terminal/starship.nix
@@ -6,6 +6,12 @@
       enable = true;
       enableZshIntegration = true;
       enableBashIntegration = true;
+      settings = {
+        character = {
+          success_symbol = "[](bold green)";
+          error_symbol = "[](bold red)";
+        };
+      };
     };
   };
 }
