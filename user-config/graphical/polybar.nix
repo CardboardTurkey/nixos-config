d4abe9e2e9f6ec9909edc4404f901a030f0289d3
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-05-12 14:53:04 +0100

    Transparent polybar

--- user-config/graphical/polybar.nix
+++ user-config/graphical/polybar.nix
@@ -10,7 +10,7 @@ let
   # Lets choose some colours!
   # Nord colours defined in `../core/nord.nix`
   foreground = "#${config.nord6}";
-  background = "#${config.nord0}";
+  background = "#00${config.nord0}";
   # Workspace colours
   ws_focused = "#${config.nord2}";
   ws_underline = "#${config.nord13}";
