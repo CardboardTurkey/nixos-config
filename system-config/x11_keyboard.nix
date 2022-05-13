d15c70f6b64d3dca49e2d734722a9efc8ef3a7ab
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-05-13 17:25:08 +0100

    WIP better keyboard layout setting maybe

--- system-config/x11_keyboard.nix
+++ system-config/x11_keyboard.nix
@@ -1,17 +1,25 @@
 { config, lib, pkgs, ... }:
 
 let
-  myCustomLayout = pkgs.writeText "xkb-layout" ''
-    keycode 51 = backslash asciitilde
-    keycode 49 = bar grave
-  '';
+  myCustomLayout = pkgs.writeText "symbols/us-kiran" ''
+      xkb_symbols "us-kiran"
+      {
+        include "us(basic)"
+        key <BKSL> {    [ backslash, asciitilde ]   };
+        key <TLDE> {    [       bar, grave      ]   };
+      };
+    '';
 in
 
 {
   environment.systemPackages = with pkgs; [
     xorg.xkbcomp
   ];
-  services.xserver.layout = "us";
   services.xserver.xkbOptions = "ctrl:nocaps";
-  services.xserver.displayManager.sessionCommands = "${pkgs.xorg.xmodmap}/bin/xmodmap ${myCustomLayout}";
+  services.xserver.extraLayouts.us-kiran = {
+    description = "US layout but a bit fucked";
+    languages   = [ "eng" ];
+    symbolsFile = "${myCustomLayout}";
+  };
+  services.xserver.layout = "us-kiran";
 }
