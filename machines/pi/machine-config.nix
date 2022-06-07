7daf166a12d99d88d253b28352ebe1e4e9df9595
Kiran Ostrolenk <kiran.ostrolenk@codethink.co.uk>
2022-06-07 19:02:56 +0100

    Access pi

--- machines/pi/machine-config.nix
+++ machines/pi/machine-config.nix
@@ -2,6 +2,7 @@
 
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  workPubSSHKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCmxAEbpaxSCoahAH0cCV+tuENZ8iJY7U+NETdHFvwmqYHGQ7aMuucy0CB8qdNTmbLfCwFoOhFMilmEru31CHaHVbJFg4+LECZC6cOOI7Wuu2PbNlVydrwErpszSVlvxyuN63ucjYVCjV0k6n5geZ59ZeTDI4KDzFNwBRnV0LRKHUVdC/TjTnPBOGl+VObyv6/WEqLP4uZEmfH4pAao7LQO8C04eknKDCOTYGDJ+6E5KScrn/9G8zIFAr1OVBjlD3y/zVZFIalfn4VkpdxV4KsW1wHXlVfMz6YioYCC0PBxtSZP0vswFrF28vxRpHPZwLnb1OLk/isx0sMmF+3cEIyE2UwsaIJaJ60hGc34EZBtitkonD1uvP/28UlKCMKV3i4iXiKIpjDFRKvH72arteIOGKBQ8JTHmgrzmsZgcolD8vieXZ+3WKax3kBVbnwK6estmrAbdQ3726bfHZOgQGmlT7sA9MV+Cqie1HG8a/49bLnE5NmS9sdZhbDloAs/UfU= kiran@finch";
 in
 
 {
@@ -10,10 +11,12 @@ in
     [ 
       (import "${home-manager}/nixos")
 
-      ../../system-config/boring_stuff.nix
       ../../system-config/pkgs_core.nix
       ../../system-config/tailscale.nix
       ../../system-config/users.nix
+      ../../system-config/network.nix
+      ../../system-config/location.nix
+      ../../system-config/openssh.nix
 
       ../../user-config/terminal/git.nix
       ../../user-config/terminal/starship.nix
@@ -37,11 +40,7 @@ in
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  networking = {
-    networkmanager = {
-      enable = true;
-    };
-  };
+  users.users.kiran.openssh.authorizedKeys.keys = [ "${workPubSSHKey}" ];
 
   # rpi3 only has 1gb ram so we need a swap file (maybe I should make it bigger )
   swapDevices = [ { device = "/swapfile"; size = 2048; } ];
