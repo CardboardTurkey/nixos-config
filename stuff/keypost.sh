541216a4cf5bfa1847494885c76dd157ce3f2224
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-07-13 09:39:29 +0100

    Better key name

--- stuff/keypost.sh
+++ stuff/keypost.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ssh-keygen -o -a 256 -t ed25519 -C "$(hostname)-$(date +'%d-%m-%Y')"
-curl -d '{"title":"test key","key":"'"$(cat ~/.ssh/id_ed25519.pub)"'"}'\
+curl -d '{"title":"'"$(hostname)-$(date +'%d-%m-%Y')"'","key":"'"$(cat ~/.ssh/id_ed25519.pub)"'"}'\
 	-H 'Content-Type: application/json'\
 	https://gitlab.com/api/v4/user/keys?private_token="$1"
