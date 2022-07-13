2583362f4b7b169468b4f639a051a43ac3916e3e
Kiran Ostrolenk <kostrolenk@gmail.com>
2022-07-13 09:39:59 +0100

    Fix for new settings option

--- user-config/graphical/picom.nix
+++ user-config/graphical/picom.nix
@@ -4,23 +4,27 @@ _:
   home-manager.users.kiran = { pkgs, ... }: {
     services.picom = {
       enable = true;
-      blur = true;
       fade = true;
       vSync = true;
-      inactiveOpacity = "0.8";
-      opacityRule = [ "100:name *= 'i3lock'" ];
-      blurExclude = [ "class_g = 'Dunst'" ];
+      inactiveOpacity = 0.8;
+      opacityRules = [ "100:name *= 'i3lock'" ];
+      backend = "glx";
       experimentalBackends = true;
-      extraOptions = ''
-        focus-exclude = [ 
-          "class_g = 'rofi'",
-          "class_g = 'polybar'",
-          "I3_FLOATING_WINDOW@:c",
-          "fullscreen",
+      settings = {
+        blur = {
+          method = "dual_kawase";
+          strength = 16;
+          background = false;
+          background-frame = false;
+          background-fixed = false;
+        };
+        focus-exclude = [
+            "class_g = 'rofi'"
+            "class_g = 'polybar'"
+            "I3_FLOATING_WINDOW@:c"
+            "fullscreen"
         ];
-        blur-method = "dual_kawase";
-        blur-strength = 16;
-      '';
+      };
     };
   };
 }
