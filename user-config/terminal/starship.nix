53ac98bedcd9cf688959ecf5fcb3e50c26b71dff
Kiran Ostrolenk <kiran.ostrolenk@codethink.co.uk>
2022-11-29 10:26:00 +0000

    Add nushell starship

--- user-config/terminal/starship.nix
+++ user-config/terminal/starship.nix
@@ -2,10 +2,13 @@ _:
 
 {
   home-manager.users.kiran = { pkgs, ... }: {
+    programs.nushell.enable = true;
+    programs.bash.enable = true;
     programs.starship = {
       enable = true;
       enableZshIntegration = true;
       enableBashIntegration = true;
+      enableNushellIntegration = true;
       settings = {
         character = {
           success_symbol = "[](bold green)";
