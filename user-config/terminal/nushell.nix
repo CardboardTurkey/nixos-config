15aca9f0a728415173168e788fe3bfebff0fa142
Kiran Ostrolenk <kiran.ostrolenk@codethink.co.uk>
2023-03-07 00:03:21 +0000

    Fix prompt

--- user-config/terminal/nushell.nix
+++ user-config/terminal/nushell.nix
@@ -9,7 +9,14 @@ in
       extraEnv = ''
         let-env MANPAGER = "sh -c 'col -bx | bat --theme Dracula -l man -p'"
         let-env PATH = ($env.PATH | append ~/.cargo/bin)
-      '';
+
+        # The prompt indicators are environmental variables that represent
+        # the state of the prompt
+        let-env PROMPT_INDICATOR = ""
+        let-env PROMPT_INDICATOR_VI_INSERT = ": "
+        let-env PROMPT_INDICATOR_VI_NORMAL = "〉"
+        let-env PROMPT_MULTILINE_INDICATOR = "::: "
+        '';
       configFile.source = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/nushell/nushell/main/crates/nu-utils/src/sample_config/default_config.nu";
         sha256 = "0h3ki6yq1yhnm7dz5gbffhyqhrvc97ajba9lpzdijnbzx03giyrc";
@@ -21,7 +28,7 @@ in
       extraConfig = ''
         use ${nu_scripts}/share/nu_scripts/git/git.nu *
         use ${nu_scripts}/share/nu_scripts/custom-completions/git/git-completions.nu *
-      '';
+        '';
     };
   };
 }
