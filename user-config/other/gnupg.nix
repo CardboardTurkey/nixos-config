c4985c2674aeb20b012770bd891a24dc3ccadf42
Kiran Ostrolenk <kiran.ostrolenk@codethink.co.uk>
2023-05-24 17:55:28 +0100

    gpg pinentry

--- user-config/other/gnupg.nix
+++ user-config/other/gnupg.nix
@@ -4,8 +4,10 @@
   # Not sure what this is
   services.pcscd.enable = true;
   hardware.gpgSmartcards.enable = true;
+  # services.yubikey-agent.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+    pinentryFlavor = "gtk2";
   };
 }
