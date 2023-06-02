9b1afc7a43e9d0b5bf163fe3a274956c40b3e8a5
Kiran Ostrolenk <kiran.ostrolenk@codethink.co.uk>
2023-06-02 16:01:28 +0100

    gpgclear service

--- user-config/other/gnupg.nix
+++ user-config/other/gnupg.nix
@@ -10,4 +10,10 @@
     enableSSHSupport = true;
     pinentryFlavor = "gtk2";
   };
+  systemd.user.services.gpgclear = {
+    enable = true;
+    after = [ "suspend.target" ];
+    path = [ pkgs.systemd ];
+    script = "systemctl --user restart gpg-agent.service";
+  };
 }
