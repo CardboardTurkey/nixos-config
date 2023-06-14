80adeeda83e00ffa2a975eaabd336225ab04fdba
Kiran Ostrolenk <kiran.ostrolenk@codethink.co.uk>
2023-06-14 08:48:28 +0200

    Update

--- system-config/pkgs_mine/nu_scripts/default.nix
+++ system-config/pkgs_mine/nu_scripts/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "nu_scripts";
-  version = "unstable-2023-06-09";
+  version = "unstable-2023-06-12";
 
   src = fetchFromGitHub {
     owner = "nushell";
     repo = "nu_scripts";
-    rev = "2e46f056c647803fb3fece3e1f00349e1f5cb1f0";
-    sha256 = "0s9vfr962rbdli6h1rgcvfdd1yv57njwmwd068vr9dah9r2g1y8y";
+    rev = "424e8d61a58563c3d6f7cec5e816924dbf69faa1";
+    sha256 = "03ijzk8b5wcwa8ama0l5vi20n3ldx54lkaydwimfjnspgm54ygid";
   };
 
   installPhase = ''
