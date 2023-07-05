7c12ccff11ef5a2acf33aa3a278f7f103ce5af81
Kiran Ostrolenk <kostrolenk@gmail.com>
2023-07-05 08:39:03 +0100

    Fix core

---
+++ core/hardware.nix
@@ -0,0 +1,47 @@
+{ lib, ... }:
+
+with lib;
+{
+  options = {
+    eth = mkOption {
+      default = "eth0";
+      type = with types; uniq str;
+      description = "Ethernet interface";
+    };
+    wlan = mkOption {
+      default = "wlan0";
+      type = with types; uniq str;
+      description = "Wireless interface";
+    };
+    battery = mkOption {
+      default = "BAT0";
+      type = with types; uniq str;
+      description = "As given in /sys/class/power_supply/";
+    };
+    adapter = mkOption {
+      default = "AC";
+      type = with types; uniq str;
+      description = "As given in /sys/class/power_supply/";
+    };
+    edp1 = mkOption {
+      default = "";
+      type = with types; uniq str;
+      description = "Fingerprint for laptop screen";
+    };
+    root = mkOption {
+      default = "";
+      type = with types; uniq str;
+      description = "Root partition id";
+    };
+    dual_monitor_left = mkOption {
+      default = "dual_monitor_left";
+      type = with types; uniq str;
+      description = "Left monitor in dual monitor setup";
+    };
+    dual_monitor_right = mkOption {
+      default = "dual_monitor_right";
+      type = with types; uniq str;
+      description = "Right monitor in dual monitor setup";
+    };
+  };
+}
