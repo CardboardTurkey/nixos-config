35c3830f49988bca675722ad6be071aa5b1c1145
Kiran Ostrolenk <kiran.ostrolenk@codethink.co.uk>
2023-08-25 17:46:03 +0100

    performance laptop

--- machines/T14/machine-config.nix
+++ machines/T14/machine-config.nix
@@ -43,6 +43,8 @@
     };
   };
 
+  powerManagement.cpuFreqGovernor = "performance";
+
   # Need fs.office.codethink.co.uk entry added to /root/.ssh/known_hosts
   # and a key without passphrase. This isn't a security issue as you need
   # to be on the office network to access fs anyway.
