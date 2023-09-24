2849075d77073cc0267ec0bf74a27bf485c7aab8
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2023-09-24 16:20:24 +0100

    Update eyegog dns

--- system-config/ayden_vpn.nix
+++ system-config/ayden_vpn.nix
@@ -7,7 +7,7 @@
     privateKeyFile = config.sops.secrets."ayden_vpn/private_key".path;
     postUp = "resolvectl dns wg0 10.10.4.254; resolvectl domain wg0 eyegog.co.uk";
     peers = [{
-      endpoint = "vpn.eyegog.co.uk:5765";
+      endpoint = "88.97.110.231:5765";
       publicKey = "rmIJKNXx9zWnMJh2e1Pbrp2ipkAyi52cYdKG0JKdf1A=";
       # publicKey = "JNg5WLd7iDDa8Fphxn6bfIBBsGOxbuzIvW62Uro4UiA=";
       allowedIPs = [ "10.10.0.0/16" ];
