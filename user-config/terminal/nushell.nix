4b889c30896de1613c42490180d905faa8920378
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2023-10-16 08:55:52 +0100

    stop updating nuscripts

--- user-config/terminal/nushell.nix
+++ user-config/terminal/nushell.nix
@@ -14,14 +14,6 @@
         $env.PROMPT_INDICATOR_VI_NORMAL = "〉"
         $env.PROMPT_MULTILINE_INDICATOR = "::: "
       '';
-      # configFile.source = pkgs.fetchurl {
-      #   url = "https://raw.githubusercontent.com/nushell/nushell/main/crates/nu-utils/src/sample_config/default_config.nu";
-      #   sha256 = "17b5n1dpqwbfzqfmii7b8qnbd585aimhp0kkq2m89hx1yk0qhjsa";
-      # };
-      # envFile.source = pkgs.fetchurl {
-      #   url = "https://raw.githubusercontent.com/nushell/nushell/main/crates/nu-utils/src/sample_config/default_env.nu";
-      #   sha256 = "17r6skzbdy2irmx1n77289prdsxky05bflfhv98z9f0hrcc4p9ka";
-      # };
       extraConfig = ''
         use ${pkgs.nu_scripts}/share/nu_scripts/modules/git/git.nu *
         use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/git/git-completions.nu *
