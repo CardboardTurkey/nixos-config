089524dd6d125b92bf4c2959d947e431d0543fbc
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2023-10-20 07:56:15 +0100

    drop boot.loader.raspberrypi

--- machines/pi/machine-config.nix
+++ machines/pi/machine-config.nix
@@ -46,12 +46,10 @@ in
     };
   };
 
-  boot.loader.raspberryPi = {
-    enable = true;
-    uboot.enable = true;
-    version = 3;
-  };
+  # NixOS wants to enable GRUB by default
   boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
 
   hardware.enableRedistributableFirmware = false;
   hardware.firmware = [ pkgs.firmwareLinuxNonfree ];
