da9e3e687becc9839142d7d103b7f1e489afc2d2
Kiran Ostrolenk <kiran.ostrolenk@codethink.co.uk>
2023-11-06 11:07:55 +0000

    regreet to greetui

--- system-config/greetd.nix
+++ system-config/greetd.nix
@@ -4,37 +4,10 @@
     enable = true;
     settings = {
       default_session = {
+        # `--remember` doesn't work and I don't understand why :(
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --cmd Hyprland";
         user = "kiran";
       };
-    };
-  };
-  programs.regreet = {
-    enable = true;
-    package = pkgs.greetd.regreet.overrideAttrs (self: super: rec {
-      version = "unstable-10-10-2032";
-      src = pkgs.fetchFromGitHub {
-        owner = "rharish101";
-        repo = "ReGreet";
-        rev = "e787317f3ddf987389017b85e089ce5a23662b6e";
-        hash = "sha256-yvQUGodhyuiXppwc8VmAIaKne4EWLXX7vEPV2zVhmSg=";
-      };
-      # cargoDeps = super.cargoDeps.overrideAttrs (_: {
-      #   inherit src;
-      #   outputHash = lib.fakeSha256;
-      # });
-    });
-
-    settings = {
-      background = {
-        path = files/nix-wallpaper-stripes-logo.png;
-        fit = "Cover";
-      };
-      # GTK = {
-      #   cursor_theme_name = "Bibata-Modern-Classic";
-      #   font_name = "Lexend 9";
-      #   icon_theme_name = "Papirus-Dark";
-      #   theme_name = "Catppuccin-Mocha-Compact-Mauve-Dark";
-      # };
     };
   };
 }
