ccc2bf682d809c72495c6d1ea156342b384beb2d
Kiran Ostrolenk <kiran.ostrolenk@codethink.co.uk>
2023-11-23 15:54:04 +0000

    devhw34

---
+++ machines/devhw34/machine-config.nix
@@ -0,0 +1,34 @@
+{ pkgs, config, ... }:
+
+{
+
+  imports =
+    [
+      ../pc_common.nix
+    ];
+
+  email = "kiran.ostrolenk@codethink.co.uk";
+  eth = "eno1";
+  root = "5f3d43dc-1955-4eae-955a-1d151d08fcfc";
+  dual_monitor_left = [ "DP-3" "DP-4" ];
+  dual_monitor_right = [ "DP-5" "DP-7" ];
+  hostname = "devhw34";
+  home-manager.users.kiran = {
+    programs.zsh.dirHashes = {
+      lorry = "$HOME/gitlab/CodethinkLabs/lorry/lorry2/";
+    };
+  };
+  wallpapers = {
+    single = "/home/kiran/Pictures/Wallpapers/ice_breaker.jpg";
+    dual = {
+      left = "/home/kiran/Pictures/Wallpapers/Polar_Bear/left.jpg";
+      right = "/home/kiran/Pictures/Wallpapers/Polar_Bear/right.jpg";
+    };
+  };
+
+  # Project change
+  boot.kernel.sysctl = { "kernel.perf_event_paranoid" = 0; };
+
+  powerManagement.cpuFreqGovernor = "performance";
+
+}
