ddbbf72bdb1504e00871ad886652aa8b8160316c
Kiran Ostrolenk <kiran.ostrolenk@codethink.co.uk>
2023-11-24 11:22:00 +0000

    notes on disabling root login

--- system-config/users.nix
+++ system-config/users.nix
@@ -9,6 +9,11 @@
     shell = pkgs.zsh;
     extraGroups = [ "kiran" "wheel" ]; # Enable ‘sudo’ for the user.
   };
-  users.groups.kiran = {};
+  users.groups.kiran = { };
   environment.shells = with pkgs; [ bash zsh ];
+
+  # https://discourse.nixos.org/t/how-to-disable-root-user-account-in-configuration-nix/13235/2
+  # Need to disable mutable users first though
+  # users.users.root.hashedPassword = "!";
+
 }
