b385fc896802b52259bceda3c0e53dcbd26b0f60
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-01-19 10:40:49 +0000

    drop devhw34 monitoring

--- machines/devhw34/machine-config.nix
+++ machines/devhw34/machine-config.nix
@@ -31,57 +31,57 @@
 
   powerManagement.cpuFreqGovernor = "performance";
 
-  # https://stackoverflow.com/questions/413807/is-there-a-way-for-non-root-processes-to-bind-to-privileged-ports-on-linux
-  # https://www.staldal.nu/tech/2007/10/31/why-can-only-root-listen-to-ports-below-1024/
-  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
+  # # https://stackoverflow.com/questions/413807/is-there-a-way-for-non-root-processes-to-bind-to-privileged-ports-on-linux
+  # # https://www.staldal.nu/tech/2007/10/31/why-can-only-root-listen-to-ports-below-1024/
+  # boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
 
-  # grafana configuration
-  services.grafana = {
-    enable = true;
-    settings = {
-      server = {
-        # Listening Address
-        http_addr = "10.35.5.148";
-        # and Port
-        http_port = 2342;
-        # Grafana needs to know on which domain and URL it's running
-        domain = "devhw34.office.codethink.co.uk";
-        root_url = "http://devhw34.office.codethink.co.uk/grafana/";
-      };
-    };
-  };
+  # # grafana configuration
+  # services.grafana = {
+  #   enable = true;
+  #   settings = {
+  #     server = {
+  #       # Listening Address
+  #       http_addr = "10.35.5.148";
+  #       # and Port
+  #       http_port = 2342;
+  #       # Grafana needs to know on which domain and URL it's running
+  #       domain = "devhw34.office.codethink.co.uk";
+  #       root_url = "http://devhw34.office.codethink.co.uk/grafana/";
+  #     };
+  #   };
+  # };
 
-  services.prometheus = {
-    enable = true;
-    port = 9001;
-    exporters = {
-      node = {
-        enable = true;
-        enabledCollectors = [ "systemd" ];
-        port = 9002;
-      };
-    };
-    scrapeConfigs = [
-      {
-        job_name = "devhw34";
-        static_configs = [{
-          targets = [ "devhw34.office.codethink.co.uk:${toString config.services.prometheus.exporters.node.port}" ];
-        }];
-      }
-    ];
-  };
+  # services.prometheus = {
+  #   enable = true;
+  #   port = 9001;
+  #   exporters = {
+  #     node = {
+  #       enable = true;
+  #       enabledCollectors = [ "systemd" ];
+  #       port = 9002;
+  #     };
+  #   };
+  #   scrapeConfigs = [
+  #     {
+  #       job_name = "devhw34";
+  #       static_configs = [{
+  #         targets = [ "devhw34.office.codethink.co.uk:${toString config.services.prometheus.exporters.node.port}" ];
+  #       }];
+  #     }
+  #   ];
+  # };
 
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
-  services.nginx.enable = true;
-  services.nginx.virtualHosts."devhw34.office.codethink.co.uk" = {
-    # addSSL = true;
-    # enableACME = true;
-    locations."/grafana/" = {
-      proxyPass = "http://${toString config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}/";
-      proxyWebsockets = true;
-      recommendedProxySettings = true;
-    };
-  };
+  # networking.firewall.allowedTCPPorts = [ 80 443 ];
+  # services.nginx.enable = true;
+  # services.nginx.virtualHosts."devhw34.office.codethink.co.uk" = {
+  #   # addSSL = true;
+  #   # enableACME = true;
+  #   locations."/grafana/" = {
+  #     proxyPass = "http://${toString config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}/";
+  #     proxyWebsockets = true;
+  #     recommendedProxySettings = true;
+  #   };
+  # };
   users.users.codething = {
     isNormalUser = true;
     home = "/home/codething";
