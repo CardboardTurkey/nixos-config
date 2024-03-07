12d5dd55367e9403eb03a034280575085de7405e
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-03-07 15:22:29 +0000

    sbuk only on t14

--- system-config/ayden_vpn.nix
+++ system-config/ayden_vpn.nix
@@ -2,7 +2,6 @@
 
 let
   eyegog_if = "eyegog";
-  sbuk_if = "sbuk";
 in
 
 {
@@ -17,19 +16,6 @@ in
       endpoint = "88.97.110.231:5765";
       publicKey = "rmIJKNXx9zWnMJh2e1Pbrp2ipkAyi52cYdKG0JKdf1A=";
       allowedIPs = [ "10.10.0.0/16" ];
-    }];
-  };
-
-  sops.secrets."sbuk/private_key" = { };
-  networking.wg-quick.interfaces.${sbuk_if} = {
-    address = [ "172.16.1.2/32" ];
-    privateKeyFile = config.sops.secrets."sbuk/private_key".path;
-    postUp = "resolvectl dns ${sbuk_if} 172.16.1.254; resolvectl domain ${sbuk_if} smoothbrained.co.uk";
-    # dns = [ "172.16.1.254" ];
-    peers = [{
-      endpoint = "128.140.103.62:9401";
-      publicKey = "3TKJwAaLM7Q32cTEMT4Mb91sDGnmnq5wjphgOxf9+kY=";
-      allowedIPs = [ "172.16.3.0/24" "172.16.100.0/24" "172.16.99.0/24" "172.16.2.0/24" "172.16.98.0/24" "172.16.1.0/24" ];
     }];
   };
 }
