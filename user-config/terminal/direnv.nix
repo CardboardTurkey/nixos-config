ddd0dc2399aa0408dc67dd9c5386259ede8e4542
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-05-12 10:51:47 +0100

    add lorri

--- user-config/terminal/direnv.nix
+++ user-config/terminal/direnv.nix
@@ -1,12 +1,15 @@
-_:
-
 {
-  home-manager.users.kiran = { pkgs, ... }: {
+  services.lorri.enable = true;
+  home-manager.users.kiran = {
     programs.direnv = {
       enable = true;
       nix-direnv.enable = true;
       enableZshIntegration = true;
       # enableNushellIntegration = true;
+    };
+    services.lorri = {
+      enable = true;
+      enableNotifications = true;
     };
   };
 }
