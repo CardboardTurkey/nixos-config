c8262d2f9b3f6f83ea34f7ca59d1fd0e991cdb06
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-05-28 10:46:41 +0100

    no mutable users

--- system-config/users.nix
+++ system-config/users.nix
@@ -1,19 +1,30 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+
+let
+  password = "passwords/${config.hostname}";
+in
 
 {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.kiran = {
-    isNormalUser = true;
-    home = "/home/kiran";
-    description = "Kiran Ostrolenk";
-    shell = pkgs.zsh;
-    extraGroups = [ "kiran" "wheel" ]; # Enable ‘sudo’ for the user.
+  imports = [ ./../user-config/other/sops.nix ];
+  sops.secrets."${password}" = { };
+
+  # Define a user account.
+  users = {
+    mutableUsers = false;
+    users = {
+      root.hashedPassword = "!";
+      kiran = {
+        isNormalUser = true;
+        home = "/home/kiran";
+        description = "Kiran Ostrolenk";
+        shell = pkgs.zsh;
+        extraGroups = [ "kiran" "wheel" ]; # Enable ‘sudo’ for the user.
+        # nix shell nixpkgs\#mkpasswd -c mkpasswd -m sha-512
+        hashedPasswordFile =
+          config.sops.secrets."${password}".path;
+      };
+    };
   };
   users.groups.kiran = { };
   environment.shells = with pkgs; [ bash zsh ];
-
-  # https://discourse.nixos.org/t/how-to-disable-root-user-account-in-configuration-nix/13235/2
-  # Need to disable mutable users first though
-  # users.users.root.hashedPassword = "!";
-
 }
