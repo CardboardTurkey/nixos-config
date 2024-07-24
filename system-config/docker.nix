45d96aa4a6aad6b11456fa181123129bf23ffbdc
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-07-24 15:42:19 +0100

    bring back rootless docker

--- system-config/docker.nix
+++ system-config/docker.nix
@@ -3,16 +3,6 @@
 {
   environment.systemPackages = with pkgs; [ docker-compose ];
 
-  home-manager.users.kiran = {
-    # programs.zsh = {
-    #   envExtra = "export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock";
-    # };
-    programs.nushell = {
-      extraEnv =
-        "$env.DOCKER_HOST = $'unix://($env.XDG_RUNTIME_DIR)/docker.sock'";
-    };
-  };
-
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
@@ -20,8 +10,9 @@
       enable = true;
       dates = "weekly";
     };
-    # rootless = {
-    #   enable = true;
-    # };
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
   };
 }
