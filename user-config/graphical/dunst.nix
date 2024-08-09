443d409cdc369bbaa355d39b61adf09992f0dd62
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-08-09 16:08:14 +0100

    try fix dunst icons

--- user-config/graphical/dunst.nix
+++ user-config/graphical/dunst.nix
@@ -128,11 +128,6 @@
   home-manager.users.kiran = { pkgs, ... }: {
     services.dunst = {
       enable = true;
-      iconTheme = {
-        name = "Zafiro-icons-Dark";
-        package = pkgs.zafiro-icons;
-        size = "32x32";
-      };
       settings = {
         global = {
           font = "Bitstream Vera Sans ${
@@ -147,6 +142,9 @@
           min_icon_size = 16;
           max_icon_size = 48;
           # enable_recursive_icon_lookup = true;
+          icon_theme = "Zafiro-icons-Dark";
+          icon_path =
+            "${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions/16:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions/16-Dark:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions/22-Dark:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions/48:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps/16:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps/48:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps/48-light:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps/scalable:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/categories:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/categories/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/categories/22-Dark:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/devices:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/devices/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/devices/48:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/emblems:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/emblems/16:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/emotes:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/mimetypes:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/mimetypes/48:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/panel:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/panel/16:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/panel/16-light:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/panel/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/panel/22-light:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/16:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/16-A:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/24:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/48:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/48-black-red:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/48-blue:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/previews:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/status:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/status/22/";
           progress_bar_height = 30;
           idle_threshold = 5;
           padding = 25;
