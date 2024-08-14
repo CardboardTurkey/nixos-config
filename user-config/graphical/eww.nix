5f5cb426b1fbb87227cd9c2d749d5ae3022957a4
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-08-14 22:26:56 +0100

    remove shitty udev rule

--- user-config/graphical/eww.nix
+++ user-config/graphical/eww.nix
@@ -1,21 +1,5 @@
 { pkgs, ... }:
-let
-  open-bar = pkgs.writeScript "eww-open-bar" ''
-    #! ${pkgs.runtimeShell}
-
-    export XDG_RUNTIME_DIR=/run/user/$UID
-
-    # For some reason the eww daemon needs to started from a genuine kiran shell
-    # - as opposed to one entered by root via `su`. No idea why.
-    timeout 10 bash -c "while ! ${pkgs.eww}/bin/eww ping;do :;done" && ${pkgs.eww}/bin/eww open bar
-  '';
-in {
-  # I don't think this rule was ever much use and is possibly slowing down shutdown.
-
-  # Would be nice if the username wasn't hard coded
-  # services.udev.extraRules = ''
-  #   ACTION=="change", SUBSYSTEM=="drm", RUN+="${pkgs.su}/bin/su kiran -c ${open-bar}"
-  # '';
+{
 
   fonts.packages = with pkgs; [ material-icons linearicons-free ];
 
