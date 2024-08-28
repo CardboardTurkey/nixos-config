5d54adf03a8dd8ec1ffa14fada43fcc7bc9ecde1
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-08-28 10:59:11 +0100

    Add jetbrains mono

--- system-config/font.nix
+++ system-config/font.nix
@@ -2,7 +2,7 @@
   allowed_unfree = [ "joypixels" ];
   nixpkgs.config.joypixels.acceptLicense = true;
   fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "DejaVuSansMono" ]; })
+    (nerdfonts.override { fonts = [ "DejaVuSansMono" "JetBrainsMono" ]; })
     hasklig
     # dejavu_fonts
     ttf_bitstream_vera
