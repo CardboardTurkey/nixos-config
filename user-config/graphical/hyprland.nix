0fb124d0e3ca26407811523e5d254ec0bab92fa1
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-09-03 16:32:27 +0100

    floating picture in picture

--- user-config/graphical/hyprland.nix
+++ user-config/graphical/hyprland.nix
@@ -290,6 +290,13 @@ in {
       windowrulev2 = [
         "float,class:(clipse)"
         "size 622 652,class:(clipse)"
+
+        "float,title:(Picture-in-Picture)"
+        "pin,title:(Picture-in-Picture)"
+        "opacity 1.0,title:(Picture-in-Picture)"
+        "size 533 300,title:(Picture-in-Picture)"
+        "move 100%-w-20 20,title:(Picture-in-Picture)"
+
         "float,class:(com.saivert.pwvucontrol)"
         "size 1200 600,class:(com.saivert.pwvucontrol)"
       ];
