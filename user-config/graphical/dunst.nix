83424228578be585c7038541aa9a081e41fb4ca3
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-09-04 16:30:44 +0100

    tweak dunst timings

--- user-config/graphical/dunst.nix
+++ user-config/graphical/dunst.nix
@@ -20,7 +20,7 @@
         icon_path =
           "${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions/16:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions/16-Dark:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions/22-Dark:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/actions/48:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps/16:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps/48:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps/48-light:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/apps/scalable:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/categories:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/categories/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/categories/22-Dark:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/devices:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/devices/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/devices/48:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/emblems:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/emblems/16:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/emotes:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/mimetypes:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/mimetypes/48:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/panel:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/panel/16:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/panel/16-light:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/panel/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/panel/22-light:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/16:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/16-A:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/22:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/24:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/48:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/48-black-red:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/places/48-blue:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/previews:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/status:${pkgs.zafiro-icons}/share/icons/Zafiro-icons-Dark/status/22/";
         progress_bar_height = 30;
-        idle_threshold = 5;
+        idle_threshold = 9;
         padding = 25;
         horizontal_padding = 25;
         corner_radius = 15;
@@ -28,12 +28,12 @@
       urgency_low = {
         background = "#${osConfig.nord0}70";
         foreground = "#${osConfig.nord5}";
-        timeout = 5;
+        timeout = 10;
       };
       urgency_normal = {
         background = "#${osConfig.nord2}70";
         foreground = "#${osConfig.nord5}";
-        timeout = 0;
+        timeout = 10;
       };
       urgency_critical = {
         background = "#${osConfig.nord11}70";
