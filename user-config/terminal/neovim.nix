751bfe6544230045e4fe79a9cfffc97798b4f73d
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-10-07 11:58:19 +0100

    fix vim commit autocomplete

--- user-config/terminal/neovim.nix
+++ user-config/terminal/neovim.nix
@@ -24,7 +24,7 @@
         nnoremap    <buffer> i  i<C-r>=<sid>commit_type()<CR>
 
         fun! s:commit_type()
-          call complete(1, ['fix: ', 'feat: ', 'refactor: ', 'docs: ', 'test: '])
+          call complete(1, ['build: ', 'chore: ', 'ci: ', 'docs: ', 'feat: ', 'fix: ', 'perf: ', 'refactor: ', 'revert: ', 'style: ', 'test: '])
           nunmap <buffer> i
           return '''
         endfun
