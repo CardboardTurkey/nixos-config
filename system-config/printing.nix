b3e79584992989619c20ad07f101ed0489b54d39
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-10-24 10:35:55 +0100

    feat: add printing auto detection

--- system-config/printing.nix
+++ system-config/printing.nix
@@ -1,4 +1,9 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [ system-config-printer ];
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 }
