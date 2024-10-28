3b359f932ec39d6b33c8087e8e0a6f15f02bc6d0
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-10-28 16:21:20 +0000

    feat: disable u2f for login

--- system-config/pam.nix
+++ system-config/pam.nix
@@ -2,6 +2,10 @@
   # Enable yubikey with `pamu2cfg > ~/.config/Yubico/u2f_keys`
   # Add second with `pamu2cfg -n >> ~/.config/Yubico/u2f_keys`
   security.pam = {
+    # Enable u2f for only sudo
+    u2f.control = "sufficient";
+    services.sudo.u2fAuth = true;
+
     # If you wanna scrap your password ()`sudo passwd -d kiran`) then you need
     # to allow null password
     # services = {
@@ -18,9 +22,5 @@
     #     allowNullPassword = true;
     #   };
     # };
-    u2f = {
-      enable = true;
-      control = "sufficient";
-    };
   };
 }
