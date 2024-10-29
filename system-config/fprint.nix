e106b0a30260aad97432da35d4eeed2017c7b0aa
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-10-29 18:35:42 +0000

    feat: fprint for xps

--- system-config/fprint.nix
+++ system-config/fprint.nix
@@ -2,5 +2,5 @@
   # Fingerprint scanning
   services.fprintd.enable = true;
   security.pam.services.login.fprintAuth = true;
-  security.pam.services.i3lock.fprintAuth = true;
+  security.pam.services.hyprlock.fprintAuth = true;
 }
