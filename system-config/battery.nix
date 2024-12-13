fa7f3263fb8aa271d066e0b601281be338d783ab
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2024-12-13 14:03:45 +0000

    feat: higher tlp thresholds

--- system-config/battery.nix
+++ system-config/battery.nix
@@ -9,8 +9,8 @@
   services.tlp = {
     enable = true;
     settings = {
-      START_CHARGE_THRESH_BAT0 = 70;
-      STOP_CHARGE_THRESH_BAT0 = 90;
+      START_CHARGE_THRESH_BAT0 = 85;
+      STOP_CHARGE_THRESH_BAT0 = 95;
     };
   };
 }
