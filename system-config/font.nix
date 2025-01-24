facbc088ad98da2c2d5084c75486f5b0b4a04b10
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-01-24 11:12:29 +0000

    fix: nerd font selection

--- system-config/font.nix
+++ system-config/font.nix
@@ -1,24 +1,35 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   allowed_unfree = [ "joypixels" ];
   nixpkgs.config.joypixels.acceptLicense = true;
-  fonts.packages = with pkgs; [
-    (nerdfonts.override {
-      fonts = [
-        "DejaVuSansMono"
-        "JetBrainsMono"
-      ];
-    })
-    # Future nixpkgs:
-    # nerd-fonts.dejavu-sans-mono
-    # nerd-fonts.jetbrains-mono
-    hasklig
-    # dejavu_fonts
-    ttf_bitstream_vera
-    joypixels
-    font-awesome
-    cantarell-fonts
-    texlivePackages.alfaslabone # for rust manchester
-    fira-sans # for rust manchester
-  ];
+  fonts.packages =
+    (
+
+      if (config.hostname == "Osprey") then
+        with pkgs;
+        [
+          (nerdfonts.override {
+            fonts = [
+              "DejaVuSansMono"
+              "JetBrainsMono"
+            ];
+          })
+        ]
+      else
+        with pkgs.nerd-fonts;
+        [
+          dejavu-sans-mono
+          jetbrains-mono
+        ]
+    )
+    ++ (with pkgs; [
+      hasklig
+      # dejavu_fonts
+      ttf_bitstream_vera
+      joypixels
+      font-awesome
+      cantarell-fonts
+      texlivePackages.alfaslabone # for rust manchester
+      fira-sans # for rust manchester
+    ]);
 }
