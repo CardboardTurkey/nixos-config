4cb3074e8ac01276f2436fcfd63cb4e94dc1aa97
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-01-24 10:57:19 +0000

    feat: switch to latest kernel for harrier

--- machines/Harrier/machine-config.nix
+++ machines/Harrier/machine-config.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 {
 
   imports = [
@@ -7,11 +7,14 @@
   ];
 
   hostname = "Harrier";
-  boot.initrd.luks.devices = {
-    crypted = {
-      device = "/dev/disk/by-partuuid/7a8eb4e4-b15e-4341-9271-5948c5ca3bc0";
-      preLVM = true;
-      allowDiscards = true;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    initrd.luks.devices = {
+      crypted = {
+        device = "/dev/disk/by-partuuid/7a8eb4e4-b15e-4341-9271-5948c5ca3bc0";
+        preLVM = true;
+        allowDiscards = true;
+      };
     };
   };
 
