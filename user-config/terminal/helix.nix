01febcc874f691bfd5ead880a0cc74e077d42458
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-01-29 14:47:02 +0000

    feat: helix lsp config

--- user-config/terminal/helix.nix
+++ user-config/terminal/helix.nix
@@ -1,5 +1,24 @@
+{ pkgs, lib, ... }:
 {
   programs.helix = {
     enable = true;
+    languages = {
+      language = [
+        {
+          name = "rust";
+          auto-format = true;
+        }
+        {
+          name = "nix";
+          auto-format = true;
+          formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+          language-servers = [ "nixd" ];
+        }
+      ];
+      language-server = {
+        rust-analyzer.config.check.command = "clippy";
+        nixd.command = lib.getExe pkgs.nixd;
+      };
+    };
   };
 }
