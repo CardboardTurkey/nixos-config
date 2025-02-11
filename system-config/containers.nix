9b87e0fda08d315d1720cc5d7f560c70518cbe59
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-02-11 11:09:36 +0000

    feat: enable containerd

---
+++ system-config/containers.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [ docker-compose ];
+
+  virtualisation = {
+    docker = {
+      enable = true;
+      enableOnBoot = true;
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+        # resolved has two different dns configs: https://unix.stackexchange.com/a/612434
+        # For some reason rootless docker picks up the wrong one,
+        # so lets just use cloudflare for now.
+        daemon.settings = {
+          dns = [ "1.1.1.1" ];
+        };
+      };
+      daemon.settings = {
+        userland-proxy = false;
+        bip = "172.26.0.1/16";
+      };
+    };
+    containerd.enable = true;
+  };
+}
