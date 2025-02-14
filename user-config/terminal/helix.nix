6c0b67ba901cd6209424ad1af1fdaabe38b78036
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-02-14 17:29:07 +0000

    feat: add markdownlint to helix

--- user-config/terminal/helix.nix
+++ user-config/terminal/helix.nix
@@ -14,11 +14,48 @@
           formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
           language-servers = [ "nixd" ];
         }
+
+        {
+          name = "markdown";
+          language-servers = [
+            # TODO: try out these other servers
+            # "vale"
+            # "marksman"
+            {
+              name = "efm";
+              only-features = [ "diagnostics" ];
+            }
+          ];
+          file-types = [
+            "md"
+            "mdx"
+          ];
+        }
       ];
       language-server = {
         rust-analyzer.config.check.command = "clippy";
+        efm.command = lib.getExe pkgs.efm-langserver;
         nixd.command = lib.getExe pkgs.nixd;
       };
+    };
+  };
+
+  xdg.configFile."efm-langserver/config.yaml".text = lib.generators.toYAML { } {
+    version = 2;
+    languages = {
+      markdown = [
+        {
+          lint-command = "${lib.getExe pkgs.markdownlint-cli} -s";
+          lint-stdin = true;
+          lint-after-open = true;
+          lint-on-save = true;
+          lint-formats = [
+            "%f:%l %m"
+            "%f:%l:%c %m"
+            "%f: %l: %m"
+          ];
+        }
+      ];
     };
   };
 }
