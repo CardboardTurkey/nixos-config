28c0f3cbb29e25cbea419144e000896ae2691ced
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-02-24 09:39:14 +0000

    build: update

--- user-config/graphical/cursor.nix
+++ user-config/graphical/cursor.nix
@@ -2,17 +2,21 @@
   pkgs,
   lib,
   osConfig,
+  options,
   ...
 }:
 let
   capitalise = word: lib.toUpper (lib.substring 0 1 word) + lib.substring 1 (-1) word;
 in
 {
-  home.pointerCursor = {
-    package = pkgs.catppuccin-cursors."${osConfig.flavour}${capitalise osConfig.accent}";
-    name = "catppuccin-${osConfig.flavour}-${osConfig.accent}-cursors";
-    gtk.enable = true;
-    hyprcursor.enable = true;
-    x11.enable = true;
-  };
+  home.pointerCursor =
+    {
+      package = pkgs.catppuccin-cursors."${osConfig.flavour}${capitalise osConfig.accent}";
+      name = "catppuccin-${osConfig.flavour}-${osConfig.accent}-cursors";
+      gtk.enable = true;
+      x11.enable = true;
+    }
+    // lib.optionalAttrs (builtins.hasAttr "hyprcursor" options.home.pointerCursor) {
+      hyprcursor.enable = true;
+    };
 }
