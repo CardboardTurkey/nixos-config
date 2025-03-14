5565de24627008d66e3b6f78ac2b4e1d9bae0f68
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-03-14 10:22:40 +0000

    feat: helix add marksman

--- user-config/terminal/helix.nix
+++ user-config/terminal/helix.nix
@@ -19,24 +19,14 @@
         {
           name = "markdown";
           language-servers = [
-            # TODO: try out these other servers
-            # "vale"
-            # "marksman"
-            {
-              name = "efm";
-              only-features = [ "diagnostics" ];
-            }
-          ];
-          file-types = [
-            "md"
-            "mdx"
+            "marksman"
           ];
         }
       ];
       language-server = {
         rust-analyzer.config.check.command = "clippy";
-        efm.command = lib.getExe pkgs.efm-langserver;
         nixd.command = lib.getExe pkgs.nixd;
+        marksman.command = lib.getExe pkgs.marksman;
       };
     };
     settings = {
@@ -63,25 +53,6 @@
           "keep_primary_selection"
         ];
       };
-    };
-  };
-
-  xdg.configFile."efm-langserver/config.yaml".text = lib.generators.toYAML { } {
-    version = 2;
-    languages = {
-      markdown = [
-        {
-          lint-command = "${lib.getExe pkgs.markdownlint-cli} -s";
-          lint-stdin = true;
-          lint-after-open = true;
-          lint-on-save = true;
-          lint-formats = [
-            "%f:%l %m"
-            "%f:%l:%c %m"
-            "%f: %l: %m"
-          ];
-        }
-      ];
     };
   };
 }
