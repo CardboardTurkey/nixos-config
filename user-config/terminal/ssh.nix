1f09ade6c67c43bde5b52d35e22b0f961ba253a2
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-03-24 11:09:37 +0000

    feat: update sbuk ssh hosts

--- user-config/terminal/ssh.nix
+++ user-config/terminal/ssh.nix
@@ -5,9 +5,21 @@
     controlPersist = "2h";
     forwardAgent = true;
     matchBlocks = {
-      "mgmt" = {
-        hostname = "www1.mgmt.smoothbrained.co.uk";
-        user = "kostrolenk";
+      "fw1" = {
+        hostname = "fw1.core.smoothbrained.co.uk";
+        user = "local-kostrolenk";
+      };
+      "fw1.mgmt.smoothbrained.co.uk" = {
+        hostname = "fw1.core.smoothbrained.co.uk";
+        user = "local-kostrolenk";
+      };
+      "www2" = {
+        hostname = "www2.mgmt.smoothbrained.co.uk";
+        user = "local-kostrolenk";
+      };
+      "www2.mgmt.smoothbrained.co.uk" = {
+        hostname = "www2.mgmt.smoothbrained.co.uk";
+        user = "local-kostrolenk";
       };
       "rpi" = {
         hostname = "100.64.201.123";
