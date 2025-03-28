abd4a6d0771bf52bbd37ea49b31a4c59aaff48dc
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-03-28 10:39:27 +0000

    feat: abandon the bleeding edge

--- machines/Harrier/machine-config.nix
+++ machines/Harrier/machine-config.nix
@@ -1,4 +1,3 @@
-{ pkgs, ... }:
 {
 
   imports = [
@@ -8,7 +7,6 @@
 
   hostname = "Harrier";
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
     initrd.luks.devices = {
       crypted = {
         device = "/dev/disk/by-partuuid/7a8eb4e4-b15e-4341-9271-5948c5ca3bc0";
