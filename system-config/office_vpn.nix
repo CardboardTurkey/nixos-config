c63dc4c219009a6c4f40e2278ab35aea9b1aeb0c
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-03-31 14:30:59 +0100

    feat: office vpn script

--- system-config/office_vpn.nix
+++ system-config/office_vpn.nix
@@ -1,9 +1,15 @@
+{ pkgs, ... }:
 {
-  services.openvpn.servers = {
-    officeVPN = {
-      config = "config /root/nixos/openvpn/officeVPN.conf ";
-      autoStart = false;
-      updateResolvConf = true;
-    };
-  };
+  # Old vpn configuration:
+  # services.openvpn.servers = {
+  #   officeVPN = {
+  #     config = "config /root/nixos/openvpn/officeVPN.conf ";
+  #     autoStart = false;
+  #     updateResolvConf = true;
+  #   };
+  # };
+
+  environment.systemPackages = [
+    (pkgs.writeScriptBin "vpnup" "pass -c vpn/sigma && nmcli c u sigma; sudo ip route delete default dev tun0")
+  ];
 }
