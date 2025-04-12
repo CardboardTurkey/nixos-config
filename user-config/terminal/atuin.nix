97be0ee7536218507540ae739f76ab96d8b9986c
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-04-12 23:21:12 +0100

    feat: atuin settings

--- user-config/terminal/atuin.nix
+++ user-config/terminal/atuin.nix
@@ -11,8 +11,10 @@
       enableZshIntegration = true;
       settings = {
         auto_sync = true;
+        filter_mode = "directory";
         sync_frequency = "5m";
         sync_address = "http://${osConfig.atuinAddress}:8888";
+        history_filter = [ ".*[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+.*" ];
       };
     }
     # daemon option not present in home-manager release 24.11
