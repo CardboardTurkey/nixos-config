70fa2af0939571f533b8f56a9ecc76ea74c428ea
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-04-17 15:14:00 +0100

    fix: bring back userland proxy for rootless docker
    absolutely no idea what this does

--- system-config/containers.nix
+++ system-config/containers.nix
@@ -20,14 +20,9 @@
         # resolved has two different dns configs: https://unix.stackexchange.com/a/612434
         # For some reason rootless docker picks up the wrong one,
         # so lets just use cloudflare for now.
-        daemon.settings = {
-          dns = [ "1.1.1.1" ];
-        };
+        daemon.settings.dns = [ "1.1.1.1" ];
       };
-      daemon.settings = {
-        userland-proxy = false;
-        bip = "172.26.0.1/16";
-      };
+      daemon.settings.bip = "172.26.0.1/16";
     };
     containerd.enable = true;
   };
