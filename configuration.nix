0f96ca7e40368121fb8f82c556611bc979822f57
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-04-25 11:07:37 +0100

    drop me

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,8 @@
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) config.allowed_unfree;
 
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 443;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
