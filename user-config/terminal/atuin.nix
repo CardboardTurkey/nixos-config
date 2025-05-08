eb208b6090eb25af189353ba9f24699de1e0c914
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-05-08 19:29:02 +0100

    feat: drop atuin daemon for a bit

--- user-config/terminal/atuin.nix
+++ user-config/terminal/atuin.nix
@@ -1,24 +1,23 @@
 {
   osConfig,
-  lib,
-  options,
+  # lib,
+  # options,
   ...
 }:
 {
-  programs.atuin =
-    {
-      enable = true;
-      enableZshIntegration = true;
-      settings = {
-        auto_sync = true;
-        filter_mode = "directory";
-        sync_frequency = "5m";
-        sync_address = "http://${osConfig.atuinAddress}:8888";
-        history_filter = [ ".*[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+.*" ];
-      };
-    }
-    # daemon option not present in home-manager release 24.11
-    // lib.optionalAttrs (builtins.hasAttr "daemon" options.programs.atuin) {
-      daemon.enable = true;
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      auto_sync = true;
+      filter_mode = "directory";
+      sync_frequency = "5m";
+      sync_address = "http://${osConfig.atuinAddress}:8888";
+      history_filter = [ ".*[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+.*" ];
     };
+    # }
+    # # daemon option not present in home-manager release 24.11
+    # // lib.optionalAttrs (builtins.hasAttr "daemon" options.programs.atuin) {
+    #   daemon.enable = true;
+  };
 }
