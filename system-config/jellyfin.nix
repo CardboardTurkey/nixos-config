a7382eb4db360c60b963fdf74112b53aecc9ac61
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-06-01 16:46:57 +0100

    feat: jellyfin caddy

--- system-config/jellyfin.nix
+++ system-config/jellyfin.nix
@@ -1,10 +1,25 @@
 { pkgs, ... }:
 {
-  services.jellyfin = {
-    enable = true;
-    openFirewall = true;
-    user = "kiran";
-    group = "kiran";
+  services = {
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+      user = "kiran";
+      group = "kiran";
+    };
+    caddy = {
+      enable = true;
+      email = "kiran@ostrolenk.co.uk";
+      extraConfig = ''
+        watch.ostrolenk.co.uk {
+          reverse_proxy localhost:8096 {
+              # Pass the original Host header from the client to Jellyfin.
+              # This is important for Jellyfin to generate correct URLs.
+              header_up Host {host}
+          }
+        }
+      '';
+    };
   };
   environment.systemPackages = with pkgs; [
     jellyfin
