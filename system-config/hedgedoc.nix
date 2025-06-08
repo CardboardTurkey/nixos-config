26b5282610595007a3630739678e7793c16d47e9
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-06-08 15:36:09 +0100

    feat: hedgedoc ldap

--- system-config/hedgedoc.nix
+++ system-config/hedgedoc.nix
@@ -1,9 +1,14 @@
+{ config, ... }:
 let
   ipAddress = "172.16.100.3";
   port = 61967;
   domain = "pad.kiran.smoothbrained.co.uk";
 in
 {
+  sops.secrets."hedgedoc" = {
+    owner = "hedgedoc";
+    group = "hedgedoc";
+  };
   networking.firewall.allowedTCPPorts = [ port ];
   services.hedgedoc = {
     enable = true;
@@ -11,21 +16,15 @@ in
       port = port;
       host = ipAddress;
       domain = domain;
-      # protocolUseSSL = true;
+      ldap = {
+        url = "ldaps://ldap.smoothbrained.co.uk";
+        # bindDn = ldap.bindDN;
+        # bindCredentials = "$bindCredentials";
+        searchBase = "ou=people,dc=smoothbrained,dc=co,dc=uk";
+        searchFilter = "(uid={{username}})";
+        useridField = "uid";
+      };
     };
   };
-  # services.nginx = {
-  #   enable = true;
-  #   virtualHosts."${domain}" = {
-  #     locations."/" = {
-  #       proxyPass = "http://${ipAddress}:${builtins.toString port}";
-  #       recommendedProxySettings = true;
-  #       proxyWebsockets = true;
-  # };
-  # locations."/socket.io/" = {
-  #   proxyPass = "http://127.0.0.1:${port}";
-  #   proxyWebsockets = true;
-  # };
-  # };
-  # };
+  systemd.services.hedgedoc.serviceConfig.EnvironmentFile = config.sops.secrets."hedgedoc".path;
 }
