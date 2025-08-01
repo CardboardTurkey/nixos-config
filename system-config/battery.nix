98bc7833f9b0d98ff82c7ef483b2207b80ab1236
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-08-01 11:10:06 +0100

    feat: disable cpu turbo on battery

--- system-config/battery.nix
+++ system-config/battery.nix
@@ -10,6 +10,10 @@
   services.tlp = {
     enable = true;
     settings = {
+      # disable turbo boost on battery
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
       START_CHARGE_THRESH_BAT0 = 85;
       STOP_CHARGE_THRESH_BAT0 = 95;
     };
