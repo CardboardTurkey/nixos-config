c2bf1e0cfe8e4239b8869ca8f7c03c5a975b7d71
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-09-11 12:25:54 +0100

    feat: future came too soon

--- system-config/greetd.nix
+++ system-config/greetd.nix
@@ -5,7 +5,9 @@
     settings = {
       default_session = {
         # `--remember` doesn't work and I don't understand why :(
-        command = "${pkgs.tuigreet}/bin/tuigreet --remember --cmd Hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --cmd Hyprland";
+        # For the future
+        # command = "${pkgs.tuigreet}/bin/tuigreet --remember --cmd Hyprland";
         user = "kiran";
       };
     };
