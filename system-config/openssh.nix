824c73520a8a9dc262382b82d6dea4ea3a22d8ea
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-10-07 12:54:36 +0100

    feat: ssh security hardening

--- system-config/openssh.nix
+++ system-config/openssh.nix
@@ -4,8 +4,19 @@
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
-    settings.StreamLocalBindUnlink = true;
+    settings = {
+      # Dont remember, probably for connection persistence
+      StreamLocalBindUnlink = true;
+      # Security hardening
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      AllowUsers = [ "kiran" ];
+    };
   };
+
+  services.fail2ban.enable = true;
+
   environment.systemPackages = with pkgs; [ waypipe ];
 
   users.users.kiran.openssh.authorizedKeys.keys = [
