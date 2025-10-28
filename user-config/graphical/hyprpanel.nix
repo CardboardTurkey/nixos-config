f4de21b6e0bace4d4808d31c2a5e294292539313
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-10-28 14:37:49 +0000

    feat: hypr support XF86AudioPause

--- user-config/graphical/hyprpanel.nix
+++ user-config/graphical/hyprpanel.nix
@@ -32,6 +32,7 @@ in
       ", XF86AudioMicMute, exec, ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
       ", XF86AudioNext, exec, ${lib.getExe pkgs.playerctl} next"
       ", XF86AudioPlay, exec, ${lib.getExe pkgs.playerctl} play-pause"
+      ", XF86AudioPause, exec, ${lib.getExe pkgs.playerctl} pause"
       ", XF86AudioPrev, exec, ${lib.getExe pkgs.playerctl} previous"
     ];
   };
