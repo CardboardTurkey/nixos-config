20208daddf3c736cf8d6a6a4ce66985425969faa
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2025-11-03 09:17:52 +0000

    build: bump hyprland

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
     apple-silicon.url = "github:tpwrules/nixos-apple-silicon";
 
-    hyprland.url = "github:hyprwm/Hyprland/v0.51.0";
+    hyprland.url = "github:hyprwm/Hyprland/v0.51.1";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
